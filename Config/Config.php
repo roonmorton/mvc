bfe668f77c1970812130d38122747f6aa18c64c2
roonmorton <roonmorton@gmail.com>
2016-01-20 23:05:30 -0600

    Actualizacion

--- Config/Config.php
+++ Config/Config.php
@@ -4,5 +4,5 @@ define('DS',DIRECTORY_SEPARATOR);
 define('DEFAULT_CONTROLLER','home');
 define('DEFAULT_TEMPLATE','template');
 define('AUTHOR', 'Ron Quevedo');
-define('SERVER','http://localhost/mvc/');
+define('SERVER','http://localhost/mvc-base-php/');
 
